3b0b3fb9941278b5b2222726beed6fc419eb80df
unknown <gabr-p@hotmail.com>
2020-02-24 21:15:17 -0500

    Version Amélioré Finalisé

--- villes.java
+++ villes.java
@@ -1,141 +1,73 @@
 package com.company;
 
-import java.text.DecimalFormat;
-
 public class villes {
 
-    public static void afficherTab(int[][] tab){
-        int nbColonnes = 0;
-        do {
-            for (int i = 0; i < tab[0].length; i++) {
-                System.out.print(tab[nbColonnes][i] + " ");
+    public static void afficherTab(int[][] tab) {
+
+
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[0].length; j++) {
+                System.out.print(tab[i][j] + " ");
             }
             System.out.println();
-            nbColonnes++;
-        } while (nbColonnes < tab.length);
+        }
     }
 
-    public static void sommeUneLigne (int[][] tab){
+    public static int sommeUneLigne(int[][] tab, int iLigne) {
 
-        int nbColonnes = 0;
+        int somme = 0;
 
-        do {
-            int total = 0;
+        for (int i = 0; i < tab[0].length; i++) {
+            somme += tab[iLigne][i];
+        }
 
-            for (int i = 0; i < tab[0].length; i++) {
-                total = total + tab[nbColonnes][i];
-            }
-            nbColonnes++;
-
-            if (nbColonnes == 1) {
-                System.out.println("Québec: " + total);
-            } else if (nbColonnes == 2) {
-                System.out.println("Toronto: " + total);
-            } else if (nbColonnes == 3) {
-                System.out.println("Joliette: " + total);
-            } else {
-                System.out.println("Gaspé: " + total);
-            }
-        } while (nbColonnes < tab.length);
+        return somme;
     }
 
-    public static double moyenneUneLigne(int[][] tab){
+    public static double moyenneUneLigne(int[][] tab, int iLigne) {
 
-        DecimalFormat df = new DecimalFormat("0.0");
-
-        int nbColonnes = 0;
         double moyenne = 0;
         double somme = 0;
 
-        do {
-            somme = somme + tab[nbColonnes][1];
-            nbColonnes++;
-        }while (nbColonnes < 4);
-        moyenne = somme /nbColonnes;
+        for (int i = 0; i < tab[0].length; i++){
+            somme += tab[iLigne][i];
+            moyenne = somme / tab[0].length;
+        }
         return moyenne;
     }
 
-    public static void moyenneUneCol(int[][] tab){
+    public static double moyenneUneCol(int[][] tab, int iCol) {
 
-        DecimalFormat df = new DecimalFormat("0.00");
-
-        int nbColonnes = 0;
         double moyenne = 0;
         double somme = 0;
 
-        do {
-            for (int i = 0; i < tab[0].length; i++) {
-                somme = somme + tab[nbColonnes][i];
-            }
-            moyenne = somme / tab[0].length;
-            nbColonnes++;
-
-            somme = 0;
-
-            if (nbColonnes == 1) {
-                System.out.println("Québec " + df.format(moyenne));
-            } else if (nbColonnes == 2) {
-                System.out.println("Toronto " + df.format(moyenne));
-            } else if (nbColonnes == 3) {
-                System.out.println("Joliette " + df.format(moyenne));
-            } else {
-                System.out.println("Gaspé " + df.format(moyenne));
-            }
-        }while (nbColonnes < tab.length);
+        for (int i = 0; i < tab.length; i++) {
+            somme += tab[i][iCol];
+            moyenne = somme / tab.length;
+        }
+        return moyenne;
     }
 
-    public static void maxLigne(int [][] tab){
 
-        int nbColonnes = 0;
-        int max = tab[nbColonnes][0];
+    public static int maxLigne(int[][] tab, int iLigne) {
 
-        do {
-            for (int i = 0; i < tab[0].length; i++){
-                if (tab[nbColonnes][i] > max){
-                    max = tab[nbColonnes][i];
-                }
-            }
-            nbColonnes++;
-            if (nbColonnes == 1) {
-                System.out.println("Québec " + max);
-                max = tab[nbColonnes][0];
-            } else if (nbColonnes == 2) {
-                System.out.println("Toronto " + max);
-                max = tab[nbColonnes][0];
-            } else if (nbColonnes == 3) {
-                System.out.println("Joliette " + max);
-                max = tab[nbColonnes][0];
-            } else {
-                System.out.println("Gaspé " + max);
-            }
+        int max = tab[iLigne][0];
 
-        }while (nbColonnes < tab.length);
+        for (int i = 0; i < tab[0].length; i++) {
+            if (tab[iLigne][i] > max)
+                max = tab[iLigne][i];
+        }
+        return max;
     }
 
-    public static void minLigne(int[][] tab){
-        int nbColonnes = 0;
-        int min = tab[nbColonnes][0];
+    public static int minLigne(int[][] tab, int iLigne) {
 
-        do {
-            for (int i = 0; i < tab[0].length; i++){
-                if (tab[nbColonnes][i] < min){
-                    min = tab[nbColonnes][i];
-                }
-            }
-            nbColonnes++;
-            if (nbColonnes == 1) {
-                System.out.println("Québec " + min);
-                min = tab[nbColonnes][0];
-            } else if (nbColonnes == 2) {
-                System.out.println("Toronto " + min);
-                min = tab[nbColonnes][0];
-            } else if (nbColonnes == 3) {
-                System.out.println("Joliette " + min);
-                min = tab[nbColonnes][0];
-            } else {
-                System.out.println("Gaspé " + min);
-            }
+        int min = tab[iLigne][0];
 
-        }while (nbColonnes < tab.length);
+        for (int i = 0; i < tab[0].length; i++){
+            if (tab[iLigne][i] < min)
+                min = tab[iLigne][i];
+        }
+        return min;
     }
 }
